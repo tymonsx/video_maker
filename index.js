0ca55313373f0720f382c19079da09c6c2c5fd16
Tymonsx <tymonsx@hotmail.com>
2020-08-08 16:02:18 -0300

    video robot

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const robots = {
   text: require("./robots/text.js"),
   state: require("./robots/state.js"),
   image: require("./robots/image.js"),
+  video: require("./robots/video.js"),
 };
 async function start() {
   //robots.input();
   //await robots.text();
-  await robots.image();
+  //await robots.image();
+  await robots.video();
 
   const content = robots.state.load();
   console.dir(content, { depth: null });
